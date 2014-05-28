469f9c8979075a5b4dd6ed038fd0264129cf5383
Nick Sutterer <apotonick@gmail.com>
2014-05-29 08:44:15 +1000

    make it work for 1.9.

--- lib/kaminari/cells.rb
+++ lib/kaminari/cells.rb
@@ -4,7 +4,7 @@ module Kaminari
 
     included do
       # compat with 3.10's ViewModel.
-      if (const_defined?("Cell::Rails::ViewModel") and self < Cell::Rails::ViewModel) or (const_defined?("Cell::ViewModel") and self < Cell::ViewModel)
+      if (::Cell::Rails.const_defined?(:ViewModel) and self < Cell::Rails::ViewModel) or (::Cell.const_defined?(:ViewModel) and self < Cell::ViewModel)
         include Kaminari::Helpers::CellsHelper
       else
         helper Kaminari::Helpers::CellsHelper
